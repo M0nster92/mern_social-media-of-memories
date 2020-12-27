3750714ba95c97ca3c845100725c86ae1c5aae9f
fahim2077 <fahimalsafi@gmail.com>
2020-12-26 23:06:07 -0500

    transfer

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import Ract from 'react';
+import useStyles from "./styles";
+
+const Form = () => {
+    const classes = useStyles();
+    return <div>Form</div>
+};
+
+export default Form;
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,9 @@
+import Ract from 'react';
+import useStyles from './styles';
+
+const Post = () => {
+    const classes = useStyles();
+    return <div>Post</div>
+};
+
+export default Post;
